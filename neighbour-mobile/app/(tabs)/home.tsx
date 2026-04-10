8c51b98d6116051433240cdda90958e281b547cd
Trick-shot <erickluoga1722@yahoo.com>
2026-04-10 23:31:12 +0300

    Added the map view

--- neighbour-mobile/app/(tabs)/home.tsx
+++ neighbour-mobile/app/(tabs)/home.tsx
@@ -1,16 +1,28 @@
-import Onboarding from "@/app/authentication/onboarding";
 import LoadingScreen from "@/components/LoadingScreen";
 import * as SplashScreen from "expo-splash-screen";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {router} from "expo-router";
-import {useEffect, useState} from "react";
+import {StatusBar} from "expo-status-bar";
+import {useCallback, useEffect, useRef, useState} from "react";
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import {View, Text, StyleSheet} from "react-native";
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 
 SplashScreen.preventAutoHideAsync();
 
 const home = () => {
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [hasUser, setHasUser] = useState(false);
+
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
+    // callbacks
+    const handleSheetChanges = useCallback((index: number) => {
+        console.log('handleSheetChanges', index);
+    }, []);
+
 
     useEffect(() => {
         checkUser()
@@ -38,7 +50,42 @@ const home = () => {
 
     if (hasUser) return null;
 
-    return <Onboarding/>;
+    return (
+        <View style={{
+            flex: 1
+        }}>
+            <StatusBar style="light" animated/>
+            <MapView provider={PROVIDER_GOOGLE} mapType="satellite" initialRegion={{
+                latitude: -6.7924,
+                longitude: 39.2083,
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05,
+            }} style={{
+                width: "100%",
+                height: "100%"
+            }}/>
+            <GestureHandlerRootView style={{
+                flex: 1,
+                ...StyleSheet.absoluteFillObject
+            }}>
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    onChange={handleSheetChanges}
+                    snapPoints={['100%']}
+                    index={0}
+                >
+                    <BottomSheetView style={{
+                        height: "20%",
+                        padding: 36,
+                        alignItems: 'center',
+                    }}>
+                        <Text>Awesome 🎉</Text>
+                    </BottomSheetView>
+                </BottomSheet>
+            </GestureHandlerRootView>
+
+        </View>
+    );
 }
 
 export default home;
